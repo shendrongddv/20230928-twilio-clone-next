4747e4fe95908715ea9306a0cfcf3d707a36143f
Kang Shend Rong <shendrongddv@gmail.com>
2023-09-28 15:48:34 +0700

    20230928 : Section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,150 @@
 import { ThemeSwitcher } from "@/components/theme-switcher";
+import { Badge } from "@/components/ui/badge";
+import { buttonVariants } from "@/components/ui/button";
+import { dummyProductDescription } from "@/config/dummy";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <ThemeSwitcher />
+
+      {/* Product description */}
+      <section className="py-16">
+        <div className="container flex flex-col items-center gap-12">
+          <h2 className="h2 text-center md:w-4/5">
+            Twilio combines the best in communications and customer data
+          </h2>
+
+          <ul className="grid w-full gap-8 md:grid-cols-3 md:gap-12">
+            {dummyProductDescription?.slice(0, 3).map((item) => (
+              <li
+                key={item.id}
+                className="gap-12 space-y-4 overflow-hidden rounded-lg border bg-background p-4 transition duration-300 hover:shadow-lg md:p-8"
+              >
+                <div className="flex items-center justify-between gap-4">
+                  <Badge className="rounded-full">{item.product}</Badge>
+
+                  <ArrowRight className="h-4 w-4 text-muted-foreground" />
+                </div>
+
+                <h3 className="h3">{item.title}</h3>
+
+                <p>{item.desc}</p>
+              </li>
+            ))}
+
+            {dummyProductDescription?.slice(3)?.map((item) => (
+              <li
+                key={item.id}
+                className="grid overflow-hidden rounded-lg border bg-background transition duration-300 hover:shadow-lg md:col-span-3 md:grid-cols-2 md:gap-12"
+              >
+                {/* Col */}
+                <div>
+                  <Image
+                    src={`/${item.media}`}
+                    alt={item.title}
+                    width={480}
+                    height={480}
+                    className="h-auto w-full"
+                  />
+                </div>
+
+                {/* Col */}
+                <div className="flex flex-col items-start justify-center space-y-4 p-4 md:p-8">
+                  <div className="flex items-center justify-between gap-4">
+                    <Badge className="rounded-full">{item.product}</Badge>
+
+                    {/* <ArrowRight className="h-4 w-4 text-muted-foreground" /> */}
+                  </div>
+
+                  <h3 className="h3">{item.title}</h3>
+
+                  <p>{item.desc}</p>
+
+                  <Link
+                    href={item.url}
+                    aria-label={item.title}
+                    className={cn(
+                      buttonVariants({
+                        variant: "ghost",
+                        size: "default",
+                        className: "p-0 hover:bg-transparent",
+                      }),
+                    )}
+                  >
+                    {item.cta}
+                    <span className="ml-2 flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-destructive text-white">
+                      <ArrowRight className="h-4 w-4" />
+                    </span>
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+      {/* ./ Product description */}
+
+      {/* Get started today */}
+      <section className="bg-slate-100 py-16">
+        <div className="container flex flex-col gap-12 text-center md:flex-row md:items-center md:justify-between md:text-start">
+          {/* Col */}
+          <div className="flex flex-col items-center md:w-5/12 md:items-start">
+            <span className="subtitle text-destructive">Get started today</span>
+
+            <h2 className="h2 mt-2">Start building with Twilio for free</h2>
+
+            <p className="lead mt-4">
+              Sign up and start building your ideal customer engagement
+              experience today.
+            </p>
+
+            <Link
+              href="/"
+              aria-label="Start for free"
+              className={cn(
+                buttonVariants({
+                  variant: "default",
+                  size: "default",
+                  className: "mt-6",
+                }),
+              )}
+            >
+              Start for free
+            </Link>
+
+            <Link
+              href="/"
+              aria-label="View pay-as-you-go pricing"
+              className={cn(
+                buttonVariants({
+                  variant: "link",
+                  size: "default",
+                  className: "mt-4 h-max p-0 underline underline-offset-8",
+                }),
+              )}
+            >
+              View pay-as-you-go pricing
+            </Link>
+          </div>
+
+          {/* Col */}
+          <div className="md:w-6/12">
+            <Image
+              src="/get-started-today.webp"
+              alt="Get started todat"
+              width={480}
+              height={480}
+              className="h-auto w-full"
+            />
+          </div>
+        </div>
+      </section>
+      {/* ./ Get started today */}
     </>
   );
 }
---
+++ config/dummy.ts
@@ -0,0 +1,39 @@
+// ProductDescription
+export const dummyProductDescription = [
+  {
+    id: "6545a3bb-77aa-5af6-b732-6b2781032080",
+    product: "Twilio CPaas",
+    title: "Reach your customers across any channel",
+    desc: "Verify, activate, and connect with global customers over SMS, voice, email, chat, and WhatsApp.",
+    media: null,
+    cta: null,
+    url: "/",
+  },
+  {
+    id: "6190d5da-b3cd-5d35-9ec0-bc966193d0d1",
+    product: "Twilio CDP",
+    title: "Know your customers personally",
+    desc: "Combine data across the customer lifecycle into a single, golden profile with our CDP.",
+    media: null,
+    cta: null,
+    url: "/",
+  },
+  {
+    id: "1f3cb96b-fdc9-5973-a564-10991921f58f",
+    product: "Twilio Flex",
+    title: "Unleash the potential of sales and support",
+    desc: "Drive revenue and retention through a powerful digital engagement center.",
+    media: null,
+    cta: null,
+    url: "/",
+  },
+  {
+    id: "468d5b1f-5d15-5490-8116-07a3c2bd9943",
+    product: "Twilio CEP",
+    title: "Twilio Customer Engagement Platform",
+    desc: "A powerful platform where customer data and customer communications meet.",
+    media: "twilio-customer-engagement-platform.webp",
+    cta: "Discover Twilio CEP",
+    url: "/",
+  },
+];
